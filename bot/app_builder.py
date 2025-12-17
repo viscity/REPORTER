7f22d210530055684ea895d21aa741d5d20b4898
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2025-12-17 14:32:24 +0530

    Update app_builder.py

--- bot/app_builder.py
+++ bot/app_builder.py
@@ -97,7 +97,7 @@ def build_app() -> Application:
         },
         fallbacks=[CommandHandler("cancel", cancel)],
         allow_reentry=True,
-        per_message=True,
+        per_message=False,
         per_chat=True,
         per_user=True,
     )
@@ -107,7 +107,7 @@ def build_app() -> Application:
         states={ADD_SESSIONS: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_added_sessions)]},
         fallbacks=[CommandHandler("cancel", cancel)],
         allow_reentry=True,
-        per_message=True,
+        per_message=False,
         per_chat=True,
         per_user=True,
     )
